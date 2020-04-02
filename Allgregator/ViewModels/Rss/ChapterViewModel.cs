9e643fc1608a07f0fe9939edc55caeba03bd716f
Makajda <mcjdaaa@gmail.com>
2020-04-02 08:55:36 +0300

    added ChapterViewModel

---
+++ Allgregator/ViewModels/Rss/ChapterViewModel.cs
@@ -0,0 +1,106 @@
+﻿using Allgregator.Common;
+using Allgregator.Models.Rss;
+using Allgregator.Services.Rss;
+using Allgregator.Views.Rss;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace Allgregator.ViewModels.Rss {
+    public class ChapterViewModel : BindableBase {
+        private readonly OreService oreService;
+        private readonly IRegionManager regionManager;
+        private readonly IEventAggregator eventAggregator;
+
+        public ChapterViewModel(
+            Chapter chapter,
+            OreService oreService,
+            IRegionManager regionManager,
+            IEventAggregator eventAggregator
+            ) {
+            Chapter = chapter;
+            this.oreService = oreService;
+            this.regionManager = regionManager;
+            this.eventAggregator = eventAggregator;
+
+            ActivateCommand = new DelegateCommand(Activate);
+            NewsCommand = new DelegateCommand(() => SetVisibleNewsOldsLinks(false, true, true));
+            OldsCommand = new DelegateCommand(() => SetVisibleNewsOldsLinks(true, false, true));
+            LinksCommand = new DelegateCommand(() => SetVisibleNewsOldsLinks(true, true, false));
+            OpenCommand = new DelegateCommand(Open);
+            DeleteCommand = new DelegateCommand(Delete);
+            UpdateCommand = new DelegateCommand(Update);
+
+            eventAggregator.GetEvent<ChapterChangedEvent>().Subscribe((chapter) => IsActive = chapter.Id == Chapter.Id);
+        }
+
+        public DelegateCommand ActivateCommand { get; private set; }
+        public DelegateCommand NewsCommand { get; private set; }
+        public DelegateCommand OldsCommand { get; private set; }
+        public DelegateCommand LinksCommand { get; private set; }
+        public DelegateCommand OpenCommand { get; private set; }
+        public DelegateCommand DeleteCommand { get; private set; }
+        public DelegateCommand UpdateCommand { get; private set; }
+
+        private Chapter chapter;
+        public Chapter Chapter {
+            get => chapter;
+            private set => SetProperty(ref chapter, value);
+        }
+
+        private bool isActive;
+        public bool IsActive {
+            get { return isActive; }
+            set { SetProperty(ref isActive, value); }
+        }
+
+        private bool isNewsVisible;
+        public bool IsNewsVisible {
+            get => isNewsVisible;
+            set => SetProperty(ref isNewsVisible, value);
+        }
+
+        private bool isOldsVisible = true;
+        public bool IsOldsVisible {
+            get => isOldsVisible;
+            set => SetProperty(ref isOldsVisible, value);
+        }
+
+        private bool isLinksVisible = true;
+        public bool IsLinksVisible {
+            get => isLinksVisible;
+            set => SetProperty(ref isLinksVisible, value);
+        }
+
+        private void SetVisibleNewsOldsLinks(bool news, bool olds, bool links) {
+            IsNewsVisible = news;
+            IsOldsVisible = olds;
+            IsLinksVisible = links;
+            SetMainView();
+        }
+
+        private void SetMainView() {
+            var type = IsNewsVisible ? (IsOldsVisible ? typeof(LinksView) : typeof(OldsView)) : typeof(NewsView);
+            regionManager.RequestNavigate(Given.MainRegion, type.Name);
+        }
+
+        private void Activate() {
+            eventAggregator.GetEvent<ChapterChangedEvent>().Publish(Chapter);
+            SetMainView();
+        }
+
+        private void Open() {
+            //TODO
+        }
+
+        private void Delete() {
+            //TODO
+        }
+
+        private async void Update() {
+            //TODO
+            //await oreService.Retrieve(CurrentChapter);
+        }
+    }
+}
--- Allgregator/ViewModels/Rss/ChaptersViewModel.cs
+++ Allgregator/ViewModels/Rss/ChaptersViewModel.cs
@@ -5,122 +5,40 @@ using Allgregator.Repositories.Rss;
 using Allgregator.Services.Rss;
 using Allgregator.Views.Rss;
 using Prism.Commands;
-using Prism.Events;
+using Prism.DryIoc;
 using Prism.Ioc;
+using Prism.Events;
 using Prism.Mvvm;
 using Prism.Regions;
-using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace Allgregator.ViewModels.Rss {
     public class ChaptersViewModel : BindableBase {
         private readonly ChapterRepository chapterRepository;
-        private readonly OreService oreService;
-        private readonly IRegionManager regionManager;
-        private readonly IEventAggregator eventAggregator;
 
         public ChaptersViewModel(
             Settings settings,
-            ChapterRepository chapterRepository,
-            OreService oreService,
-            IRegionManager regionManager,
-            IEventAggregator eventAggregator
+            ChapterRepository chapterRepository
             ) {
             this.chapterRepository = chapterRepository;
-            this.oreService = oreService;
-            this.regionManager = regionManager;
-            this.eventAggregator = eventAggregator;
-
-            ActivateCommand = new DelegateCommand<Chapter>(Activate);
-            NewsCommand = new DelegateCommand(News);
-            OldsCommand = new DelegateCommand(Olds);
-            LinksCommand = new DelegateCommand(Links);
-            OpenCommand = new DelegateCommand(Open);
-            DeleteCommand = new DelegateCommand(Delete);
-            UpdateCommand = new DelegateCommand(Update);
 
             var chapters = chapterRepository.GetChapters();
             if (chapters != null) {
-                Chapters = new ObservableCollection<Chapter>(chapters);
+                var container = (App.Current as PrismApplication).Container;
+                Chapters = new ObservableCollection<ChapterViewModel>(chapters.Select(n => container.Resolve<ChapterViewModel>((typeof(Chapter), n))));
 
-                var currentChapter = Chapters.FirstOrDefault(n => n.Id == settings.RssChapterId);
+                var currentChapter = Chapters.FirstOrDefault(n => n.Chapter.Id == settings.RssChapterId);
                 if (currentChapter == null) {
                     currentChapter = Chapters.FirstOrDefault();
                 }
 
-                Activate(currentChapter);
+                if (currentChapter != null) {
+                    currentChapter.IsActive = true;
+                }
             }
         }
 
-        public DelegateCommand<Chapter> ActivateCommand { get; private set; }
-        public DelegateCommand NewsCommand { get; private set; }
-        public DelegateCommand OldsCommand { get; private set; }
-        public DelegateCommand LinksCommand { get; private set; }
-        public DelegateCommand OpenCommand { get; private set; }
-        public DelegateCommand DeleteCommand { get; private set; }
-        public DelegateCommand UpdateCommand { get; private set; }
-
-        public ObservableCollection<Chapter> Chapters { get; private set; }
-
-        private Chapter currentChapter;
-        public Chapter CurrentChapter {
-            get => currentChapter;
-            set => SetProperty(ref currentChapter, value);
-        }
-
-        private bool isNewsVisible;
-        public bool IsNewsVisible {
-            get => isNewsVisible;
-            set => SetProperty(ref isNewsVisible, value);
-        }
-
-        private bool isOldsVisible = true;
-        public bool IsOldsVisible {
-            get => isOldsVisible;
-            set => SetProperty(ref isOldsVisible, value);
-        }
-
-        private bool isLinksVisible = true;
-        public bool IsLinksVisible {
-            get => isLinksVisible;
-            set => SetProperty(ref isLinksVisible, value);
-        }
-
-        private void Activate(Chapter chapter) {
-            CurrentChapter = chapter;
-            eventAggregator.GetEvent<ChapterChangedEvent>().Publish(chapter);
-        }
-
-        private void News() {
-            IsNewsVisible = false;
-            IsOldsVisible = true;
-            IsLinksVisible = true;
-            regionManager.RequestNavigate(Given.MainRegion, typeof(NewsView).Name);
-        }
-
-        private void Olds() {
-            IsNewsVisible = true;
-            IsOldsVisible = false;
-            IsLinksVisible = true;
-            regionManager.RequestNavigate(Given.MainRegion, typeof(OldsView).Name);
-        }
-
-        private void Links() {
-            IsNewsVisible = true;
-            IsOldsVisible = true;
-            IsLinksVisible = false;
-            regionManager.RequestNavigate(Given.MainRegion, typeof(LinksView).Name);
-        }
-
-        private void Open() {
-        }
-
-        private void Delete() {
-        }
-
-        private async void Update() {
-            await oreService.Retrieve(CurrentChapter);
-        }
+        public ObservableCollection<ChapterViewModel> Chapters { get; private set; }
     }
 }
