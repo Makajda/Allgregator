d6a5b8dc14baef1c37e66e487d41b530bda22000
Makajda <mcjdaaa@gmail.com>
2020-04-26 10:41:20 +0300

    set main button

---
+++ Allgregator.Fin/Services/OreService.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Allgregator.Fin.Services {
+    internal class OreService : BindableBase {
+
+        private bool isRetrieving;
+        public bool IsRetrieving {
+            get { return isRetrieving; }
+            set { SetProperty(ref isRetrieving, value); }
+        }
+    }
+}
--- Allgregator.Fin/ViewModels/ChaptersViewModel.cs
+++ Allgregator.Fin/ViewModels/ChaptersViewModel.cs
@@ -1,13 +1,23 @@
-﻿using Prism.Commands;
+﻿using Allgregator.Fin.Services;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Allgregator.Fin.ViewModels {
-    public class ChaptersViewModel : BindableBase {
-        public ChaptersViewModel() {
+    internal class ChaptersViewModel : BindableBase {
+        public ChaptersViewModel(
+            OreService oreService
+            ) {
+            OreService = oreService;
+        }
+        public OreService OreService { get; private set; }
 
+        private bool isActive;
+        public bool IsActive {
+            get => isActive;
+            set => SetProperty(ref isActive, value);
         }
     }
 }
