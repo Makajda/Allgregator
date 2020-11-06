23017696554e6f698571a3eff0b30fd147537194
Vladimir Makayda <bob300@mail.ru>
2020-11-06 18:44:36 +0300

    colors

--- Allgregator/MainWindowViewModel.cs
+++ Allgregator/MainWindowViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using Prism.Mvvm;
 
-namespace Allgregator {
-    public class MainWindowViewModel : BindableBase {
+namespace Allgregator
+{
+    public class MainWindowViewModel : BindableBase
+    {
         public MainWindowViewModel(
-            ) {
+            )
+        {
         }
 
         private string title = "Получение информации";
-        public string Title {
+        public string Title
+        {
             get { return title; }
             set { SetProperty(ref title, value); }
         }
