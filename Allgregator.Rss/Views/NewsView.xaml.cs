e7c8468456031fc046a9b9c27ed8d0242f7daeec
makajda <mcjdaaa@gmail.com>
2026-01-23 22:20:56 +0300

    WebView2

--- Allgregator.Rss/Views/NewsView.xaml.cs
+++ Allgregator.Rss/Views/NewsView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using Allgregator.Rss.Models;
+using Prism.Commands;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Allgregator.Rss.Views {
     /// <summary>
@@ -7,6 +10,21 @@ namespace Allgregator.Rss.Views {
     public partial class NewsView : UserControl {
         public NewsView() {
             InitializeComponent();
+            OpenCommand = new DelegateCommand<Reco>(Open);
+        }
+
+        public DelegateCommand<Reco> OpenCommand { get; private set; }
+
+        private async void Open(Reco reco) {
+            Window window = new() {
+                ShowActivated = true, ShowInTaskbar = false, Title = reco.ItemTitle,
+                Width = 1400, Height = 1000, WindowStartupLocation = WindowStartupLocation.CenterScreen, Owner = Application.Current.MainWindow
+            };
+            Microsoft.Web.WebView2.Wpf.WebView2 browser = new();
+            window.Content = browser;
+            window.Show();
+            await browser.EnsureCoreWebView2Async();
+            browser.NavigateToString(reco.SummaryHtml);
         }
     }
 }
